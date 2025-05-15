4a974715e7ca73658faade3d6bba2c26635b9520
LoremSol <lorenzogomezdepablo@gmail.com>
2025-05-15 10:19:58 +0200

    Añadido botón para generar y guardar el documento

--- to2_sale_pdfs/models/to2_sale_order.py
+++ to2_sale_pdfs/models/to2_sale_order.py
@@ -1,3 +1,5 @@
+import base64
+
 from odoo import models, fields
 
 class SaleOrder(models.Model):
@@ -7,5 +9,20 @@ class SaleOrder(models.Model):
         'ir.attachment',
         'res_id',
         domain=[('res_model', '=', 'sale.order')],
-        string='Documentos adjuntos'
     )
+
+
+    def save_report_as_attachment(self):
+        self.ensure_one()
+        report = self.env.ref('sale.action_report_saleorder')
+        pdf_content, _ = report._render_qweb_pdf(report.id)
+        attachment_vals = {
+            'name': f'Order_{self.name}.pdf',
+            'type': 'binary',
+            'datas': base64.b64encode(pdf_content),
+            'res_model': self._name,
+            'res_id': self.id,
+            'mimetype': 'application/pdf',
+        }
+        attachment = self.env['ir.attachment'].create(attachment_vals)
+        return attachment
